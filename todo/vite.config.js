bb23577a7ee49994b5ea56d6bee093f6de1b3d2d
kyung <rhrud0412@naver.com>
2024-12-06 13:16:19 +0900

    배포 셋팅

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '../goorm_study/todo/',
-})
+  base: '/goorm_study/todo/', // GitHub Pages에서 이 프로젝트의 경로와 일치
+  build: {
+    outDir: '../dist/todo', // 빌드 결과물을 저장할 경로
+    emptyOutDir: false, // 기존 내용 삭제하지 않음
+  },
+});
