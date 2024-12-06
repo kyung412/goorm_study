a65ff74d4cff280f24d83a01bfadee6e0de7ac3a
kyung <rhrud0412@naver.com>
2024-12-06 14:41:22 +0900

    삭제

--- todo/src/component/TodoInput.jsx
+++ todo/src/component/TodoInput.jsx
@@ -28,6 +28,8 @@ const TodoInput = ({ addTodo }) => {
    * - 사용자가 Enter 키를 누르면 handleAdd 함수 호출
    */
   const handleKeyDown = (e) => {
+    if (e.keyCode === 229) return;
+    // 맥에서 한글을 입력하는 동작(onKeyDown/Up)에서 함수 콜링이 두번 중첩되는 이슈가 있어 해결책 삽입
     if (e.key === 'Enter') handleAdd(); //Enter키를 누를 때만 실행
   };
 
